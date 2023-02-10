f0ef03c91dbdeefab47ab50f6e8492101929384c
Amneet Kaur <111182139+amneetkaur25@users.noreply.github.com>
2023-02-10 20:16:23 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+#eeXOmDlG1ATya/gLjZJ8bQ==CB5iysUzR5SZFFhh
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    import requests
+    import json
+    if request.method=="POST":
+        query=request.POST.get('query')
+  
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+        response = requests.get(api_url, headers={'X-Api-Key': 'eeXOmDlG1ATya/gLjZJ8bQ==CB5iysUzR5SZFFhh'})
+        try:
+            api=json.loads(response.content)
+        except Exception as e:
+            api="Opps there was an error!"
+            print(e)
+        return render(request,"home.html",{'api':api})
+    else:
+        return render(request,"home.html",{'query':"Enter a Valid Query"})
+
